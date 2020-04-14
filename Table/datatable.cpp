938970826fd07fcec25d4f8aee1ad5664e2b32cf
shuangshaohen <yu_chuankun@126.com>
2020-04-14 21:53:45 +0800

    Table的示例程序

---
+++ Table/datatable.cpp
@@ -0,0 +1,54 @@
+﻿#include "datatable.h"
+#include <QDebug>
+
+DataTable::DataTable(QWidget *parent) : QTableWidget(parent)
+{
+    resize(800,600);
+
+    m_datas.clear();
+    for (int i = 0; i < 10; i++) {
+        tdsData * p = new tdsData(i);
+        m_datas.push_back(p);
+    }
+
+    update();
+}
+
+void DataTable::printInfo()
+{
+    for (int i = 0; i < 10; i++) {
+        qDebug() << m_datas.at(i)->info();
+    }
+}
+
+void DataTable::spinBoxValChanged(int val)
+{
+    int * p = (int *)QObject::sender()->property("point").toUInt();
+    *p = val;
+}
+
+void DataTable::update()
+{
+    clearContents();
+    setRowCount(m_datas.size());
+    setColumnCount(4);
+
+    for (int i = 0; i < 10; i++) {
+        //ID
+        createSpinBox(i);
+    }
+
+}
+
+void DataTable::createSpinBox(int row)
+{
+    QSpinBox * pSpinBox = new QSpinBox();
+    pSpinBox->setAlignment(Qt::AlignHCenter|Qt::AlignVCenter);
+    pSpinBox->setRange(0,65535);
+    pSpinBox->setProperty("point",(uint)&m_datas[row]->id);
+    pSpinBox->setValue(m_datas[row]->id);
+    pSpinBox->setSingleStep(2);
+    connect(pSpinBox, SIGNAL(valueChanged(int)),this,SLOT(spinBoxValChanged(int)));
+
+    setCellWidget(row,EN_DATATABLE_COLUMN_ID,pSpinBox);
+}
---
+++ Table/datatable.h
@@ -0,0 +1,60 @@
+﻿#ifndef DATATABLE_H
+#define DATATABLE_H
+
+#include <QWidget>
+#include <QTableWidget>
+#include <QSpinBox>
+
+struct tdsData
+{
+    int     id;
+    QString name;
+    float   val;
+    bool    check;
+
+    tdsData(int num)
+    {
+        id = num;
+        name = "Data" + QString::number(num);
+        val = (float)num/10;
+        check = false;
+    }
+
+    QString info()
+    {
+        QString info = QString("id = %1, name = %2, val = %3, check = %4 ").arg(id).arg(name).arg(val).arg(check);
+        return info;
+    }
+};
+
+class DataTable : public QTableWidget
+{
+    Q_OBJECT
+
+    enum _EN_DATATABLE_COLUMN_
+    {
+        EN_DATATABLE_COLUMN_ID = 0,
+        EN_DATATABLE_COLUMN_NAME,
+        EN_DATATABLE_COLUMN_VAL,
+        EN_DATATABLE_COLUMN_CHECK,
+    };
+
+public:
+    explicit DataTable(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+    void printInfo();
+
+protected slots:
+    void spinBoxValChanged(int val);
+private:
+    QVector<tdsData *>  m_datas;
+
+    void    update();
+
+    void    createSpinBox(int row);
+};
+
+#endif // DATATABLE_H
